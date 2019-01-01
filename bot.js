faa79b5e0c58776ec0920145eab4e04aefa7338e
mazinmikaelsd <46274738+mazinmikaelsd@users.noreply.github.com>
2019-01-01 07:51:45 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,32 +84,11 @@ if (message.content === '!spam') {
 
 
 
-
-client.on('message', message => {
-        var prefix = "$";
-        if (message.author.bot) return;
-        if (!message.content.startsWith(prefix)) return;
-      
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-      
-      
-      let args = message.content.split(" ").slice(1);
-      let x = args.join(" ")
-        if(message.content.startsWith(prefix + 'Say')) {
-            message.channel.send(''+x);
-                message.delete(999)
-        }
-        
-       
-      });
-
-
-
-
-
-
-
+client.on('ready', () => {//Toxic Codes
+    console.log(`Logged in as ${client.user.tag} !`);//Toxic Codes
+          client.user.setActivity("Flox  .",{type: 'WATCHING'});//Toxic Codes
+  //Toxic Codes
+  });//Toxic Codes
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
